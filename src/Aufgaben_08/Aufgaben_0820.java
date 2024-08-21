6dbff3c3095f35faa01f60d7ea7e8f4af9fb097d
spielgrund <138134622+spielgrund@users.noreply.github.com>
2024-08-21 11:08:36 +0200

    0820

--- src/Aufgaben_08/Aufgaben_0820.java
+++ src/Aufgaben_08/Aufgaben_0820.java
@@ -2,24 +2,28 @@ package Aufgaben_08;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Aufgaben_0820 {
 
-    static void findAllJavaFiles(String start) throws IOException {
+    static void findAllJavaFiles(String start, String relativeString) throws IOException {
         File file = new File(start);
         File[] files = file.listFiles();
 
         for(File f: files){
             if(f.toString().endsWith(".java")){
-                System.out.println(f.getCanonicalPath());
+                Path p = Paths.get(f.getCanonicalPath());
+                Path relativePath = Paths.get(relativeString);
+                System.out.println(relativePath.relativize(p));
             }
             if(f.isDirectory()){
-                findAllJavaFiles(f.toString());
+                findAllJavaFiles(f.toString(),relativeString);
             }
         }
     }
 
     public static void main(String[] args) throws IOException {
-        findAllJavaFiles("C:\\Users\\timme\\IdeaProjects\\OCAStudyGuide\\src\\Aufgaben_08");
+        findAllJavaFiles("C:\\Users\\timme\\IdeaProjects\\OCAStudyGuide\\src\\Aufgaben_08","C:\\Users\\timme\\IdeaProjects\\OCAStudyGuide\\src\\" );
     }
 }
