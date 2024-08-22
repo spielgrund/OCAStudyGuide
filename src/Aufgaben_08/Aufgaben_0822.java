04a90d1dcb7ddf82104de8701d280ec7cc3825e8
spielgrund <138134622+spielgrund@users.noreply.github.com>
2024-08-22 13:57:32 +0200

    0822

--- src/Aufgaben_08/Aufgaben_0822.java
+++ src/Aufgaben_08/Aufgaben_0822.java
@@ -5,28 +5,27 @@ package Aufgaben_08;
 // Das Programm soll mit den folgenden Programmparametern aufgerufen werden können:
 //ein.txt aus.txt IDO-8859-1 UTF-8
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.Charset;
 
 public class Aufgaben_0822 {
 
     public static void txtConverter(String input, String output, String inputFormat, String outputFormat)
             throws FileNotFoundException {
         FileInputStream fileInputStream = new FileInputStream(input);
+        InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, Charset.forName(inputFormat));
         FileOutputStream fileOutputStream = new FileOutputStream(output);
+        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream,Charset.forName(outputFormat));
         try{
-            int data;
-
-            byte[] block = new byte[1024]; // Blockgröße von 1024 Bytes
+            char[] charbuffer = new char[1024];
             int bytesRead;
-            while ((bytesRead = fileInputStream.read(block)) != -1) {
-                fileOutputStream.write(block, 0, bytesRead);
+            while ((bytesRead = inputStreamReader.read(charbuffer)) != -1) {
+
+                outputStreamWriter.write(charbuffer,0,bytesRead);
             }
 
-            fileInputStream.close();
-            fileOutputStream.close();
+            inputStreamReader.close();
+            outputStreamWriter.close();
 
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -36,6 +35,6 @@ public class Aufgaben_0822 {
     }
 
     public static void main(String[] args) throws FileNotFoundException {
-        txtConverter("0822_input.txt","0822_output.txt", "", "");
+        txtConverter("0822_input.txt","0822_output.txt", "UTF-8", "ISO-8859-1");
     }
 }
